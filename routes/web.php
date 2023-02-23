49e680d6641a07fdb740420e3a81d3b261f0da79
hurihmeranti <hurihmeranti@gmail.com>
2023-02-23 18:42:52 +0700

    phase 3

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/home', function () {
+    return view('home');
 });
+
+?>
